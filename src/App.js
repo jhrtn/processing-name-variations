19c623d224e10dc37262e2809146dc9ce4dff08f
Joseph Horton <josephhorton95@gmail.com>
2021-08-20 17:16:42 +0100

    do the thing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
+const letters = {
+  p: ['?'],
+  r: ['2'],
+  o: ['0',],
+  c: ['(', '[', '<', '{'],
+  e: ['3', '&', '£', '€'],
+  s: ['$', '5'],
+  i: ['1', '!', '|'],
+  n: ['n'],
+  g: ['&', '6', '9'],
+}
+
+const flip = () => Math.random() >= 0.5;
+
 function App() {
+  const [processing, setProcessing] = useState('Processing');
+  const regenerateText = () => {
+    const res = Array.from("processing").map((letter) => {
+      const leetOptions = letters[letter];
+      return flip() ? leetOptions[Math.floor(Math.random()*leetOptions.length)] : letter;
+    })
+    
+    setProcessing(res);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div>
+        <h1>{processing}</h1>
+        <button onClick={regenerateText} >regenerate!</button>
+      </div>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  background-color: #F0EDEE;
+  color: #0A090C;
+
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+
+  h1 {
+    font-variant-numeric: tabular-nums;
+    font-family: monospace;
+  }
+
+  button {
+    outline: none;
+    border: none;
+    background-color: #07393C;
+    color: #F0EDEE;
+    padding: 12px 20px;
+    border-radius: 2px;
+
+    :hover {
+      transform: scale(1.01);
+    }
+  }
+`;
