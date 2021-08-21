6d3634b36283c5c7a563756e2503c7f37bf620bb
Joseph Horton <josephhorton95@gmail.com>
2021-08-21 15:02:41 +0100

    refactor classes into own files

--- src/p5/sketch.js
+++ src/p5/sketch.js
@@ -1,4 +1,7 @@
-const processingColours = ['#0a47ff', '#171d9a', '#709dff'];
+import { Bubble } from './bubble';
+import { Repeller } from './repel';
+
+export const processingColours = ['#0a47ff', '#171d9a', '#709dff'];
 
 export default function sketch(p5) {
   let bubbles = [];
@@ -57,114 +60,4 @@ export default function sketch(p5) {
       }
     });
   };
-}
-
-class Bubble {
-  constructor(_p5, _x, _y, _size, _parent) {
-    this.p5 = _p5;
-    this.pos = this.p5.createVector(_x, _y);
-    this.vel = this.p5.createVector(0, 0.2);
-    this.acc = this.p5.createVector(0, 0);
-    this.maxSpeed = 4;
-
-    this.col =
-      processingColours[Math.floor(Math.random() * processingColours.length)];
-    this.life = _size;
-    this.mass = _size;
-    this.burst = false;
-    this.parent = _parent;
-    this.decreaseRate = 4.0;
-    this.children = [];
-
-    if (this.parent) {
-      for (let i = 0; i < 5; i++) {
-        this.children.push(
-          new Bubble(
-            this.p5,
-            // these positions are the initial, needs to be the latest position
-            this.p5.random(this.pos.x - 10, this.pos.x + 10),
-            this.p5.random(this.pos.y - 10, this.pos.y + 10),
-            this.p5.random(40, 60),
-            false
-          )
-        );
-      }
-    } else {
-      this.decreaseRate = 2.0;
-    }
-  }
-
-  draw() {
-    if (this.life > 0) {
-      this.vel.add(this.acc);
-      this.pos.add(this.vel);
-      this.acc.set(0, 0);
-      // this.vel.limit(2);
-    }
-
-    this.p5.noStroke();
-    this.p5.fill(this.col);
-
-    if (this.parent) {
-      if (this.life > 0) {
-        this.p5.circle(this.pos.x, this.pos.y, this.life);
-        this.children.forEach((child) => child.setRandomPos(this.pos));
-      } else {
-        this.children.forEach((child, index) => {
-          if (!child.burst) {
-            child.draw();
-          } else {
-            this.children.splice(index, 1);
-          }
-        });
-      }
-      if (this.life <= 0 && this.children.length === 0) {
-        this.burst = true;
-      }
-    } else if (!this.parent) {
-      if (this.life > 0) {
-        this.p5.circle(this.pos.x, this.pos.y, this.life);
-      } else {
-        this.burst = true;
-      }
-    }
-
-    this.life -= this.decreaseRate;
-  }
-
-  applyForce(force) {
-    let f = this.p5.constructor.Vector.div(force, this.mass);
-    this.acc.add(f);
-  }
-
-  setRandomPos(parentPos) {
-    this.pos.x = this.p5.random(parentPos.x - 30, parentPos.x + 30);
-    this.pos.y = this.p5.random(parentPos.y - 30, parentPos.y + 30);
-  }
-}
-
-class Repeller {
-  constructor(_p5, _x, _y, _mass = 100) {
-    this.p5 = _p5;
-    this.mass = _mass;
-    this.pos = this.p5.createVector(_x, _y);
-  }
-
-  draw() {
-    this.p5.noStroke();
-    this.p5.fill(180);
-    this.p5.circle(this.pos.x, this.pos.y, this.mass);
-  }
-
-  repel(bubble) {
-    if (!bubble.burst) {
-      let force = this.p5.constructor.Vector.sub(this.pos, bubble.pos);
-      let distSq = this.p5.constrain(force.magSq(), 30, 200);
-      let G = 0.7;
-      let strength = (G * (this.mass * bubble.mass)) / distSq;
-      // let strength = G / distSq;
-      force.setMag(strength * -1);
-      bubble.applyForce(force);
-    }
-  }
 }
